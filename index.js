399659a5f4900dd36685685545180936cb37f643
Your Name <you@example.com>
2020-03-24 21:48:12 -0500

    more generalized sorta working form losses new pics on second submission

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import * as state from "./store";
+import { Home } from "./store";
 import { StateSubject } from "./lib/StateSubject";
 import { NavigatableObserver } from "./lib/NavigatableObserver";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import { BlogObserver } from "./lib/BlogObserver";
+import { FormObserver } from "./lib/FormObserver";
 
 const router = new Navigo(window.location.origin);
 
-const appState = new StateSubject(state.Home);
+const appState = new StateSubject(Home);
+// create a page set for the website, as state changes all of these
+// will update via the notification mechanism of the Subject/Observer
+// design pattern
 const pages = [];
-pages.push(new NavigatableObserver(appState, state.Bio));
-pages.push(new NavigatableObserver(appState, state.Form));
-pages.push(new BlogObserver(appState, state.Blog));
-pages.push(new NavigatableObserver(appState, state.Gallery));
-pages.push(new NavigatableObserver(appState, state.Home));
+pages.push(new NavigatableObserver(appState, appState.stateStore.Bio)); //state.Bio));
+pages.push(new FormObserver(appState, appState.stateStore.Form)); // state.Form));
+pages.push(new BlogObserver(appState, appState.stateStore.Blog)); // state.Blog));
+pages.push(new NavigatableObserver(appState, appState.stateStore.Gallery)); // state.Gallery));
+pages.push(new NavigatableObserver(appState, appState.stateStore.Home)); // state.Home));
 
 //when state changes, we use a clear "setState" instead of the abiguois "render"
 router
   .on({
-    ":page": params => appState.setState(state[capitalize(params.page)]),
-    "/": () => appState.setState(state.Home)
+    ":page": params =>
+      appState.setState(appState.stateStore[capitalize(params.page)]),
+    "/": () => appState.setState(appState.stateStore.Home)
   })
   .resolve();
