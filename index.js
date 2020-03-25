539318c152f7a5e3f8f57c6c660ad69719f2c154
Your Name <you@example.com>
2020-03-24 20:26:42 -0500

    not quite working  "The message port closed before a response was received." this pointer undefined...

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ import { StateSubject } from "./lib/StateSubject";
 import { NavigatableObserver } from "./lib/NavigatableObserver";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
-import { BlogObserver } from "./lib/BlogObserver";
+//import { BlogObserver } from "./lib/BlogObserver";
+import { BlogPublisherObserver } from "./lib/BlogPublisherObserver";
+import { BlogPublisher } from "./lib/BlogPublisher";
+import { BlogSubscriber } from "./lib/BlogSubscriber";
 
 const router = new Navigo(window.location.origin);
 
 const appState = new StateSubject(state.Home);
+
+const blogPublisher = new BlogPublisher();
+
+const blogPubObserver = new BlogPublisherObserver(appState, state.Blog);
+
+const blogSubscriber = new BlogSubscriber(blogPublisher, blogPubObserver);
+blogSubscriber.subscribe();
+blogPublisher.intiateRequest();
+
 const pages = [];
 pages.push(new NavigatableObserver(appState, state.Bio));
 pages.push(new NavigatableObserver(appState, state.Form));
-pages.push(new BlogObserver(appState, state.Blog));
+pages.push(blogPubObserver); //new BlogObserver(appState, state.Blog));
 pages.push(new NavigatableObserver(appState, state.Gallery));
 pages.push(new NavigatableObserver(appState, state.Home));
 
