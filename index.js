6e9740ade9c2901a8f00c730f28ebfc39135dbfc
Your Name <you@example.com>
2020-03-26 00:46:06 -0500

    cleanup: - verify no need for refresh in api/blog for lng running requests

--- index.js
+++ index.js
@@ -11,25 +11,13 @@ import {
 const router = new Navigo(window.location.origin);
 
 const appState = new StateSubject(state.Home, state, router);
-// create a page set for the website, as state changes all of these
-// will update via the notification mechanism of the Subject/Observer
-// design pattern
-// router
-//   .on({
-//     ":page": params =>
-//       appState.setState(appState.stateStore[capitalize(params.page)]),
-//     "/": () => appState.setState(appState.stateStore.Home)
-//   })
-//   .resolve();
 
 const pages = [];
-// pages.push(new StateObserver(appState, appState.stateStore.Bio));
-// pages.push(new StateObserver(appState, appState.stateStore.Home));
-pages.push(new NavigatableObserver(appState, appState.stateStore.Bio)); //state.Bio));
-pages.push(new FormObserver(appState, appState.stateStore.Form)); // state.Form));
-pages.push(new BlogObserver(appState, appState.stateStore.Blog)); // state.Blog));
-pages.push(new NavigatableObserver(appState, appState.stateStore.Gallery)); // state.Gallery));
-pages.push(new NavigatableObserver(appState, appState.stateStore.Home)); // state.Home));
+pages.push(new NavigatableObserver(appState, appState.stateStore.Bio));
+pages.push(new FormObserver(appState, appState.stateStore.Form));
+pages.push(new BlogObserver(appState, appState.stateStore.Blog));
+pages.push(new NavigatableObserver(appState, appState.stateStore.Gallery));
+pages.push(new NavigatableObserver(appState, appState.stateStore.Home));
 
 //when state changes, we use a clear "setState" instead of the abiguois "render"
 router
