946ff0c9109a6e79b436cffa6ffaccb91ca4b743
Your Name <you@example.com>
2020-03-26 00:00:54 -0500

    this was the cause: this.subject.router.updatePageLinks();

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import * as state from "./store";
 import {
   StateSubject,
   NavigatableObserver,
   BlogObserver,
   FormObserver
 } from "./lib";
-// import { StateSubject } from "./lib/StateSubject";
-// import { NavigatableObserver } from "./lib/NavigatableObserver";
-// import { BlogObserver } from "./lib/BlogObserver";
-// import { FormObserver } from "./lib/FormObserver";
 
 const router = new Navigo(window.location.origin);
 
-const appState = new StateSubject(state.Home, state);
+const appState = new StateSubject(state.Home, state, router);
 // create a page set for the website, as state changes all of these
 // will update via the notification mechanism of the Subject/Observer
 // design pattern
+// router
+//   .on({
+//     ":page": params =>
+//       appState.setState(appState.stateStore[capitalize(params.page)]),
+//     "/": () => appState.setState(appState.stateStore.Home)
+//   })
+//   .resolve();
+
 const pages = [];
+// pages.push(new StateObserver(appState, appState.stateStore.Bio));
+// pages.push(new StateObserver(appState, appState.stateStore.Home));
 pages.push(new NavigatableObserver(appState, appState.stateStore.Bio)); //state.Bio));
 pages.push(new FormObserver(appState, appState.stateStore.Form)); // state.Form));
 pages.push(new BlogObserver(appState, appState.stateStore.Blog)); // state.Blog));
